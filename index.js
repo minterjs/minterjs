c768f4b8c9ca06ebeba5e5e0319e8b7579822005
Vladimir V. Myagdeev <vvmspace@gmail.com>
2019-06-01 16:56:45 +0500

    Minter Network API GET Query skeleton + peers function as example

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
+const axios = require('axios');
+const log = require('./libs/log');
+
 class MinterJS {
-    constructor (auth = {}){
+    constructor(auth = {}) {
         this.host = auth.host || 'http://localhost:8841';
         this.tx = auth.tx || 0.1;
     }
+
+    async get({path, data}){
+        const url = `${this.host}/${path}`;
+        let result = await axios.get(url, data).catch(err => log(err => log(e.message)));
+        return result && result.data || null;
+    }
+
+    async peers(){
+        const {result} = await this.get({path: 'net_info'});
+        return result && result.peers || [];
+    }
 }
 
-module.exports = MinterJS;
+module.exports = (auth = {}) => {return new MinterJS(auth);};
